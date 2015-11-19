a48a741305b196b0ba29e4269ce003445f0d116c
Serhii Zinchuk <ferbolg@gmail.com>
2015-11-19 02:05:18 +0200

    Corrected spelling error for field name

--- app/models/Cat.scala
+++ app/models/Cat.scala
@@ -10,7 +10,7 @@ class PersonsTable(tag: Tag) extends Table[Person](tag, "Person") {
 
   def name = column[String]("name")
   def surname = column[String]("surname")
-  def email = column[String]("color", O.NotNull)
+  def email = column[String]("email", O.NotNull)
   def pk = primaryKey("pk_person", (name, surname))
 
   def * = (name,surname, email) <> (Person.tupled, Person.unapply _)
